6ec6fbf1590a887c9ecef7a5364c30933103b71c
jyuesong <jyuesong@gmail.com>
2016-05-20 11:25:57 +0800

    Initial commit

---
+++ app/src/main/java/com/jiang/android/translatetoast/ListenClipboardService.java
@@ -0,0 +1,129 @@
+package com.jiang.android.translatetoast;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.text.TextUtils;
+
+import com.jiang.android.translatetoast.clipboard.ClipboardManagerCompat;
+
+
+public final class ListenClipboardService extends Service implements TipViewController.ViewDismissHandler {
+
+    private static final String KEY_FOR_WEAK_LOCK = "weak-lock";
+    private static final String KEY_FOR_CMD = "cmd";
+    private static final String KEY_FOR_CONTENT = "content";
+    private static final String CMD_TEST = "test";
+
+    private static CharSequence sLastContent = null;
+    private ClipboardManagerCompat mClipboardWatcher;
+    private TipViewController mTipViewController;
+    private ClipboardManagerCompat.OnPrimaryClipChangedListener mOnPrimaryClipChangedListener = new ClipboardManagerCompat.OnPrimaryClipChangedListener() {
+        public void onPrimaryClipChanged() {
+            performClipboardCheck();
+        }
+    };
+
+    public static void start(Context context) {
+        Intent serviceIntent = new Intent(context, ListenClipboardService.class);
+        context.startService(serviceIntent);
+    }
+
+    /**
+     * for dev
+     */
+    public static void startForTest(Context context, String content) {
+
+        Intent serviceIntent = new Intent(context, ListenClipboardService.class);
+        serviceIntent.putExtra(KEY_FOR_CMD, CMD_TEST);
+        serviceIntent.putExtra(KEY_FOR_CONTENT, content);
+        context.startService(serviceIntent);
+    }
+
+    public static void startForWeakLock(Context context, Intent intent) {
+
+        Intent serviceIntent = new Intent(context, ListenClipboardService.class);
+        context.startService(serviceIntent);
+
+        intent.putExtra(ListenClipboardService.KEY_FOR_WEAK_LOCK, true);
+        Intent myIntent = new Intent(context, ListenClipboardService.class);
+
+        // using wake lock to start service
+        WakefulBroadcastReceiver.startWakefulService(context, myIntent);
+    }
+
+    @Override
+    public void onCreate() {
+        mClipboardWatcher = ClipboardManagerCompat.create(this);
+        mClipboardWatcher.addPrimaryClipChangedListener(mOnPrimaryClipChangedListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mClipboardWatcher.removePrimaryClipChangedListener(mOnPrimaryClipChangedListener);
+
+        sLastContent = null;
+        if (mTipViewController != null) {
+            mTipViewController.setViewDismissHandler(null);
+            mTipViewController = null;
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Utils.printIntent("onStartCommand", intent);
+
+        if (intent != null) {
+            // remove wake lock
+            if (intent.getBooleanExtra(KEY_FOR_WEAK_LOCK, false)) {
+                BootCompletedReceiver.completeWakefulIntent(intent);
+            }
+            String cmd = intent.getStringExtra(KEY_FOR_CMD);
+            if (!TextUtils.isEmpty(cmd)) {
+                if (cmd.equals(CMD_TEST)) {
+                    String content = intent.getStringExtra(KEY_FOR_CONTENT);
+                    showContent(content);
+                }
+            }
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void performClipboardCheck() {
+        CharSequence content = mClipboardWatcher.getText();
+        if (TextUtils.isEmpty(content)) {
+            return;
+        }
+        showContent(content);
+    }
+
+    private void showContent(CharSequence content) {
+        if (sLastContent != null && sLastContent.equals(content) || content == null) {
+            return;
+        }
+        sLastContent = content;
+
+        if (mTipViewController != null) {
+            mTipViewController.updateContent(content);
+        } else {
+            mTipViewController = new TipViewController(getApplication(), content);
+            mTipViewController.setViewDismissHandler(this);
+            mTipViewController.show();
+        }
+    }
+
+    @Override
+    public void onViewDismiss() {
+        sLastContent = null;
+        mTipViewController = null;
+    }
+}
---
+++ app/src/main/java/com/jiang/android/translatetoast/MainActivity.java
@@ -0,0 +1,55 @@
+package com.jiang.android.translatetoast;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String KEY_CONTENT = "content";
+    private TextView mTextView;
+
+    public static void startForContent(Context context, String content) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(KEY_CONTENT, content);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text_view);
+
+        Intent intent = getIntent();
+        Utils.printIntent("MainActivity::onCreate()", intent);
+
+        tryToShowContent(intent);
+        ListenClipboardService.start(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        Utils.printIntent("MainActivity::onNewIntent()", intent);
+
+        tryToShowContent(intent);
+    }
+
+    private void tryToShowContent(Intent intent) {
+        String content = intent.getStringExtra(KEY_CONTENT);
+        if (!TextUtils.isEmpty(content)) {
+            mTextView.setText(content);
+        }
+    }
+
+
+}
